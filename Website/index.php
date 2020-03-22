6f83379dd09dabaaf3565698658301b81c80d34e
ChrisJMolloy <cmolloy22@gmail.com>
2020-03-22 08:42:39 -0400

    editing page

--- Website/index.php
+++ Website/index.php
@@ -4,66 +4,24 @@
 include('header.html');
 //connection to DB
 include('connection.php');
-/*
-//always use '' for echo's, keep things consistent
-echo'
-<div class="container">
-<div class="row">
-<div class="col-4"></div>
-<div class="col-4">
 
-
-
-';
-
-$sql = "SELECT animal_id, species FROM animal";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    echo '
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Animal ID</th>
-      <th scope="col">Species</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-    
-    ';
-
-
-
-
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo '<tr><td>' . $row['animal_id']. '</td><td> ' . $row['species']. '</td></tr> ';
-    }
-    echo' </tbody>
-        </table>';
-} else {
-    echo "0 results";
-}
-$conn->close();
-echo'</div>
-<div class="col-4"></div>
-
-
-</div>
-
-</div>'*/
 ?>
+<style>
+  html, body {margin: 0; height: 100%; overflow: hidden}
+</style>
 <header class="bg-primary">
-        <div class="container h-100">
+        <div class="h-100">
             <div class="row h-100">
-                <div class="col-12">
-                    <div class="text-center m-0 vh-100 d-flex flex-column justify-content-center text-light">
+            <div class="col-4"></div>
+            <div class="col-4"></div>
+            <!--FIX THIS FOR ALL WINDOW SIZES-->
+            <div class="col-4" style="padding-top :120px; margin-left:-80px;"> <div class="m-0 vh-100 d-flex flex-column justify-content-center text-light">
                         <h1 class="display-3" style="font-weight: 500">SAVE A LIFE TODAY!</h1>
-                        <p class="lead" style="font-weight: 500">Browse our Available Pets below</p>
                         
                         
-                    </div>
+                        
+                    </div></div>
+
                 </div>
             </div>
         </div>
