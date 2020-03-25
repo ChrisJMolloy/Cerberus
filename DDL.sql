d119fec20d351b51e5d699bd54330935aded6d82
Ross Hill <rosshill@protonmail.com>
2020-03-25 12:38:56 -0400

    Make driver page & fix typo in DDL

--- DDL.sql
+++ DDL.sql
@@ -1,35 +1,39 @@
-CREATE TABLE organization(
+CREATE TABLE organization
+(
     organization_id int NOT NULL,
-    organization_name varchar(255),
-    organization_street varchar(255),
-    organization_city varchar(255),
-    organization_province varchar(255),
-    organization_postal_code varchar(255),
+    organization_name varchar(191),
+    organization_street varchar(191),
+    organization_city varchar(191),
+    organization_province varchar(191),
+    organization_postal_code varchar(191),
     organization_phone BIGINT(11),
     PRIMARY KEY (organization_id)
- 
-   
+
+
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee
+(
     employee_id int NOT NULL,
     organization_id int NOT NULL,
-    employee_fname varchar(255),
-    employee_lname varchar(255),
+    employee_fname varchar(191),
+    employee_lname varchar(191),
     employee_phone BIGINT(11),
-    employe_addr varchar(255),
+    employe_addr varchar(191),
     PRIMARY KEY (employee_id),
     FOREIGN KEY (organization_id) REFERENCES organization(organization_id) ON DELETE cascade
 );
 
-CREATE TABLE spca(
+CREATE TABLE spca
+(
     organization_id int NOT NULL,
     PRIMARY KEY (organization_id),
     FOREIGN KEY (organization_id) REFERENCES organization(organization_id) ON DELETE CASCADE
 
 );
 
-CREATE TABLE rescue(
+CREATE TABLE rescue
+(
     organization_id int NOT NULL,
     owner_id int,
     PRIMARY KEY (organization_id),
@@ -38,45 +42,50 @@ CREATE TABLE rescue(
 
 );
 
-CREATE TABLE shelter(
+CREATE TABLE shelter
+(
     organization_id int NOT NULL,
     owner_id int,
-    website varchar(255),
+    website varchar(191),
     PRIMARY KEY (organization_id),
     FOREIGN KEY (organization_id) REFERENCES organization(organization_id)  ON DELETE CASCADE,
     FOREIGN KEY (owner_id) REFERENCES employee(employee_id) ON DELETE SET NULL
 
 );
 
-CREATE TABLE donation(
+CREATE TABLE donation
+(
     donator_id int NOT NULL,
     organization_id int NOT NULL,
     donation_date DATE NOT NULL,
     donation_amount FLOAT(2),
-    donation_fname varchar(255),
-    donation_lname varchar(255),
+    donation_fname varchar(191),
+    donation_lname varchar(191),
     PRIMARY KEY (donator_id,donation_date),
     FOREIGN KEY (organization_id) REFERENCES organization(organization_id) ON DELETE cascade
 
 );
 
-CREATE TABLE animal(
+CREATE TABLE animal
+(
     animal_id int NOT NULL,
-    species varchar(255),
+    species varchar(191),
     PRIMARY KEY (animal_id)
 );
 
-CREATE TABLE vet(
+CREATE TABLE vet
+(
     vet_id int NOT NULL,
-    name varchar(255),
+    name varchar(191),
     PRIMARY KEY (vet_id)
 );
 
-CREATE TABLE vet_visit(
+CREATE TABLE vet_visit
+(
     vet_visit_id int NOT NULL,
     animal_id int NOT NULL,
     vet_id int NOT NULL,
-    animal_condition varchar(255),
+    animal_condition varchar(191),
     animal_weight_lbs FLOAT(2),
     checkup_date DATE,
     PRIMARY KEY (vet_visit_id),
@@ -84,17 +93,19 @@ CREATE TABLE vet_visit(
     FOREIGN KEY (vet_id) REFERENCES vet(vet_id) ON DELETE CASCADE
 );
 
-CREATE TABLE family(
+CREATE TABLE family
+(
     family_id int NOT NULL,
-    family_lname varchar(255),
-    family_street varchar(255),
-    family_city varchar(255),
-    family_province varchar(255),
-    family_postal_code varchar(255),
+    family_lname varchar(191),
+    family_street varchar(191),
+    family_city varchar(191),
+    family_province varchar(191),
+    family_postal_code varchar(191),
     PRIMARY KEY (family_id)
 );
 
-CREATE TABLE adoption(
+CREATE TABLE adoption
+(
     adoption_id int NOT NULL,
     family_id int NOT NULL,
     animal_id int NOT NULL,
@@ -103,7 +114,8 @@ CREATE TABLE adoption(
     FOREIGN KEY (family_id) REFERENCES family(family_id) ON DELETE CASCADE
 );
 
-CREATE TABLE visit(
+CREATE TABLE visit
+(
     visit_id int NOT NULL,
     organization_id int NOT NULL,
     animal_id int NOT NULL,
@@ -113,31 +125,45 @@ CREATE TABLE visit(
     FOREIGN KEY (organization_id) REFERENCES organization(organization_id) ON DELETE CASCADE
 );
 
-CREATE TABLE animal_types(
-    species varchar(255) NOT NULL,
+CREATE TABLE animal_types
+(
+    species varchar(191) NOT NULL,
     organization_id int NOT NULL,
-    capacity int unsigned NOT NULL,
-    PRIMARY KEY (species),
-    FOREIGN KEY (organization_id) REFERENCES shelter (organization_id) ON DELETE CASCADE
+    capacity int
+    unsigned NOT NULL,
+    PRIMARY KEY
+    (species),
+    FOREIGN KEY
+    (organization_id) REFERENCES shelter
+    (organization_id) ON
+    DELETE CASCADE
 );
 
-CREATE TABLE driver(
-    driver_id int NOT NULL,
-    organization_id int,
-    driver_fname varchar(255) NOT NULL,
-    driver_lname varchar(255) NOT NULL,
-    driver_phone BIGINT(11),
-    driver_lisence_plate varchar(255) UNIQUE,
-    driver_lisence_number int unsigned UNIQUE,
-    PRIMARY KEY (driver_id),
-    FOREIGN KEY (organization_id) REFERENCES rescue (organization_id) ON DELETE SET NULL
-);
+    CREATE TABLE driver
+    (
+        driver_id int NOT NULL,
+        organization_id int,
+        driver_fname varchar(191) NOT NULL,
+        driver_lname varchar(191) NOT NULL,
+        driver_phone BIGINT(11),
+        driver_license_plate varchar(191) UNIQUE,
+        driver_license_number int
+        unsigned UNIQUE,
+    PRIMARY KEY
+        (driver_id),
+    FOREIGN KEY
+        (organization_id) REFERENCES rescue
+        (organization_id) ON
+        DELETE
+        SET NULL
+        );
 
-CREATE TABLE animal_transfer(
-    animal_transfer_id int NOT NULL,
-    driver_id int,
-    animal_id int NOT NULL,
-    PRIMARY KEY (animal_transfer_id),
-    FOREIGN KEY (animal_id) REFERENCES animal(animal_id) ON DELETE CASCADE,
-    FOREIGN KEY (driver_id) REFERENCES driver (driver_id) ON DELETE SET NULL
-);
+        CREATE TABLE animal_transfer
+        (
+            animal_transfer_id int NOT NULL,
+            driver_id int,
+            animal_id int NOT NULL,
+            PRIMARY KEY (animal_transfer_id),
+            FOREIGN KEY (animal_id) REFERENCES animal(animal_id) ON DELETE CASCADE,
+            FOREIGN KEY (driver_id) REFERENCES driver (driver_id) ON DELETE SET NULL
+        );
---
+++ Website/People/drivers.php
@@ -0,0 +1,98 @@
+<?php
+//header for all pages
+//always put   </body> </html> at the end of a file
+include('../header.html');
+//connection to DB
+include('../connection.php');
+
+//always use '' for echo's, keep things consistent
+echo '
+<div class="container" style="padding-top:20px;">
+    <div class="row" style="padding-bottom:20px;">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <h1>Find Drivers from Organization</h1>
+        </div>
+        <div class="col-2"></div>
+    </div>
+<!-- beginning of form-->   
+    <form  action="" method="post">
+        <!-- beginning of input-->
+
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8">
+
+                <div class="form-group">
+                    <label for="vet_name">
+                        <h3>Organization Name:</h3>
+                    </label>
+                    <select class = "form-control" name = "org_id">';
+
+foreach ($dbh ->query('SELECT organization_name, organization.organization_id FROM organization join rescue on rescue.organization_id = organization.organization_id') as $row) {
+    echo "<option value='" . $row["organization_id"] . "'>" . $row["organization_name"] . "</option>";
+};
+
+echo '
+                    </select>
+                </div>
+
+            </div>
+            <div class="col-2"></div>
+        </div>
+
+<!--end of input-->
+
+<!--beginning of input-->
+<div class="row">
+    <div class="col-2"></div>
+    <div class="col-8">
+
+    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+
+    </div>
+    <div class="col-2"></div>
+</div>
+
+
+<!--end of input-->
+</form>
+<!-- end of form-->
+<div class="row">
+    <div class="col-2"></div>
+    <div class="col-8">
+    <table>
+        <thead>
+            <tr>
+                <th>
+                Name
+                </th>
+                <th>
+                Phone number
+                </th>
+                <th>
+                License plate
+                </th>
+                <th>
+                License number
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+';
+
+if(isset($_POST["submit"])){
+    $organizationId = $_POST['org_id'];
+    foreach($dbh->query("SELECT driver_fname, driver_lname, driver_phone, driver_license_plate, driver_license_number FROM driver where driver.organization_id = $organizationId") as $row) {
+        echo "<tr><td>" . $row["driver_fname"] ." ". $row["driver_lname"] . "</td><td>" . $row["driver_phone"] . "</td><td>" . $row["driver_license_plate"] . "</td><td>" . $row["driver_license_number"] . "</td></tr>";
+    }
+
+}
+
+echo '</tbody></table></div><div class="col-2"></div></div>';
+
+?>
+
+</body>
+
+</html>
